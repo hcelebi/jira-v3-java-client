166659b8294fd42cd5796bdc84d2d1f7ef8ea451
Hakan Celebi <hakan.celebi@turk.net>
2023-11-29 15:26:42 +0300

    add ignore annotation

--- src/main/java/io/github/hcelebi/jirav3/domain/response/search/SearchResult.java
+++ src/main/java/io/github/hcelebi/jirav3/domain/response/search/SearchResult.java
@@ -1,5 +1,6 @@
 package io.github.hcelebi.jirav3.domain.response.search;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import io.github.hcelebi.jirav3.domain.response.issue.Issue;
 import lombok.Data;
 
@@ -8,6 +9,7 @@ import java.io.Serializable;
 import java.util.List;
 
 @Data
+@JsonIgnoreProperties(ignoreUnknown = true)
 public class SearchResult implements Serializable {
     @Serial
     private static final long serialVersionUID = -3979685397403719193L;
