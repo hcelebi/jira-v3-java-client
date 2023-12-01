3b2918501133a0a5ecc1630e386cfd27bc935758
Hakan Celebi <hakan.celebi@turk.net>
2023-12-01 10:43:19 +0300

    add issue status field

--- src/main/java/io/github/hcelebi/jirav3/domain/request/GetSearchResultRequest.java
+++ src/main/java/io/github/hcelebi/jirav3/domain/request/GetSearchResultRequest.java
@@ -1,17 +1,21 @@
 package io.github.hcelebi.jirav3.domain.request;
 
+import lombok.Builder;
 import lombok.Data;
 
 import java.io.Serial;
 import java.io.Serializable;
 
 @Data
+@Builder
 public class GetSearchResultRequest implements Serializable {
 
     @Serial
     private static final long serialVersionUID = 5102154650035149868L;
 
     private String jql;
-    private Integer startAt;
-    private Integer maxResults;
+    @Builder.Default
+    private Integer startAt = 0;
+    @Builder.Default
+    private Integer maxResults = 100;
 }
--- src/main/java/io/github/hcelebi/jirav3/domain/response/issue/Issue.java
+++ src/main/java/io/github/hcelebi/jirav3/domain/response/issue/Issue.java
@@ -1,14 +1,18 @@
 package io.github.hcelebi.jirav3.domain.response.issue;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import lombok.Data;
 
 import java.io.Serial;
 import java.io.Serializable;
 
 @Data
+@JsonIgnoreProperties(ignoreUnknown = true)
 public class Issue implements Serializable {
     @Serial
-    private static final long serialVersionUID = -6546278080526723073L;
-    private String summary;
+    private static final long serialVersionUID = -2187175487844074608L;
+    private String id;
+    private String self;
     private String key;
+    private IssueFields fields;
 }
