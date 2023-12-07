164ba33cc64ca404b0c483cdd4e89e50d84dc166
Hakan Celebi <hakan.celebi@turk.net>
2023-12-07 19:36:06 +0300

    add issue sprint field

--- src/main/java/io/github/hcelebi/jirav3/domain/response/issue/IssueSprint.java
+++ src/main/java/io/github/hcelebi/jirav3/domain/response/issue/IssueSprint.java
@@ -1,5 +1,6 @@
 package io.github.hcelebi.jirav3.domain.response.issue;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
 import io.github.hcelebi.jirav3.serializer.CustomDateTimeDeserializer;
 import lombok.Data;
@@ -9,6 +10,7 @@ import java.io.Serial;
 import java.io.Serializable;
 
 @Data
+@JsonIgnoreProperties(ignoreUnknown = true)
 public class IssueSprint implements Serializable {
     @Serial
     private static final long serialVersionUID = -7400710475373442615L;
